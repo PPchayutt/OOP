6290e092b6bab0866e5b02e28343cdfa223de7e2
PPchayutt <67070115@kmitl.ac.th>
2024-12-14 12:39:55 +0700

    add

---
+++ NetBeansProjects/Lab3/src/demo.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author pitch
+ */
+import java.util.*;
+
+public class demo {
+
+    public static void main(String[] args) {
+        int min = 10;
+        int max = 20;
+        for (int i = 1; i < min; i++) {
+            System.out.println("X");
+        }
+    }
+
+}
