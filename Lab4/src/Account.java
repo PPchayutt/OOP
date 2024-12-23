b86eac2ef6210dd0af50b1d1cff97d8542d5ddaf
PPchayutt <67070115@kmitl.ac.th>
2024-12-23 23:21:50 +0700

    fix lab4

--- Lab4/src/Account.java
+++ Lab4/src/Account.java
@@ -13,27 +13,30 @@ public class Account {
     public String name;
 
     public void deposit(double b) {
-        if (b < 0) {
-            System.out.println("The balance variable must be greater than or equal to zero.");
-            return;
+        if (b >= 0) {
+            balance += b;
+        } else {
+            System.out.println("The balance variable must be greather than"
+                    + " or equal to zero.");
         }
-        balance += b;
     }
 
     public double withdraw(double b) {
-        if (b < 0) {
-            System.out.println("The balance variable must be greater than or equal to zero.");
+        if (balance < 0) {
+            System.out.println("The balance variable must be greather than "
+                    + "or equal to zero.");
             return 0;
-        }
-        if (balance < b) {
+        } else if (b <= balance & b >= 0) {
+            balance -= b;
+            return balance;
+        } else {
             System.out.println("Your account balance is insufficient.");
             return 0;
         }
-        balance -= b;
-        return b;
     }
 
     public void showInfo() {
-        System.out.println("In " + name + " account, there is a balance equal to " + balance + " baht.");
+        System.out.println("In " + name + " account, there is a balance equal "
+                + "to " + balance + " baht. ");
     }
 }
